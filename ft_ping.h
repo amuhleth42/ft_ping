c6b8ebb6b76a7812d2c004d37d9955f81a113829
Aymeric Muhlethaler <amuhleth@c3r3s5.42lausanne.ch>
2025-08-23 21:49:38 +0200

    Add ft_ping.h, start V1

---
+++ ft_ping.h
@@ -0,0 +1,13 @@
+#ifndef FT_PING_H
+# define FT_PING_H
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <arpa/inet.h>
+
+
+#endif
---
+++ test1.c
@@ -0,0 +1,10 @@
+#include "ft_ping.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 1)
+	{
+		printf("ft_ping: usage error: Destination address required");
+		return (1);
+	}
+}
